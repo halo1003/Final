db33c07523cdeaeeab222933a88de91c00eca0ae
halo1003 <do.toan95@gmai.com>
2017-08-02 15:02:44 +0800

    Login.js

--- mWinvest_Renew/src/components/Login.js
+++ mWinvest_Renew/src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View,Text,TextInput,ScrollView } from 'react-native';
+import { View,Text,TextInput,ScrollView,Image } from 'react-native';
 import {connect} from 'react-redux';
 import { setUsername, setPassword, login } from '../actions';
+import { Title, Header, Body, Item,Input, Right,Button,Icon,Spinner } from 'native-base';
 
 const StatusBar = require('../containers/StatusBar');
 const ActionButton = require('../containers/ActionButton');
@@ -12,7 +13,7 @@ class Login extends Component{
     super(props);
     this.state = {
       username:'',
-      password:'',
+      password:''
     };
   }
 
@@ -22,37 +23,49 @@ class Login extends Component{
 
   render() {
     return (
-      <ScrollView>
         <View style={styles.container}>
-          <View>
-            <StatusBar title="Login Form" />
-          </View>
-
-          <View style={styles.box}>
-            <TextInput
-              onChangeText={(value) => this.setState({username: value})}
-              placeholder="Username"
-            />
-
-            <TextInput
-              onChangeText={(value) => this.setState({password: value})}
-              placeholder="Password"
-            />
-          </View>
-
-        <View style={styles.box}>
-          <ActionButton onPress= {this.onLogin} title="Login">
-          </ActionButton>
+        <View style={{paddingRight:10}}>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
         </View>
-      </View>
-    </ScrollView>
+
+          <View style = {styles.logoContainer}>
+            <Image source = {require('../images/ttl_logo.png')}/>
+            <Text style = {{paddingTop:10}}>An app is using React Native</Text>
+          </View>
+
+          <View style={{padding:20}}>
+            <Item>
+              <Icon active name='person' />
+              <Input style={{fontSize:15}}
+                  placeholder='Username'
+                  onChangeText={(value) => this.setState({username: value})}/>
+            </Item>
+
+            <Item>
+              <Icon active name='lock' />
+              <Input style={{fontSize:15}}
+                  placeholder='Password'
+                  onChangeText={(value) => this.setState({password: value})}/>
+            </Item>
+
+          </View>
+          <View style={{margin:20}}>
+            {this.props.authorizing ?  <Spinner color='blue' />:
+              this.props.notexist ? <Button block onPress= {this.onLogin} style={{alignSelf: 'stretch',alignItems:'center'}}><Text style={{color:'white'}}>Login</Text></Button>:
+              <Button block onPress= {this.onLogin} style={{alignSelf: 'stretch',alignItems:'center'}}><Text style={{color:'white'}}>Login</Text></Button>}
+          </View>
+        </View>
     );
   }
 }
 
 const mapStateToProps = (state) =>{
   return{
-    authorizing: state.userReducer.authorizing
+    authorizing: state.userReducer.authorizing,
+    authorized:state.userReducer.authorized,
+    notexist:state.userReducer.notexist
   }
 }
 
