03e8bdc8e7f2f0bdaba64c6b1126f0f75b1c84b7
halo1003 <do.toan95@gmai.com>
2017-08-21 12:27:39 +0800

    last

--- mWinvest_Renew/src/reducers/navigatorReducer.js
+++ mWinvest_Renew/src/reducers/navigatorReducer.js
@@ -1,33 +1,99 @@
-import { START_STOCK_LIST, START_ORDER, START_PORTFOLIO} from '../actions/types'
-
 const initialState = {
-  StockList: false,
-  Order: false,
-  Portfolio:false
+  leftHeader: 'Watchlists',
+  rightHeader: 'Edit',
+  bodyNumber: 1,
 };
 
 const navigatorReducer = (state = initialState,action) =>{
+  //console.log('called');
   switch (action.type) {
-    case START_STOCK_LIST:
+    case 1:
       return Object.assign({},state,{
-        StockList: true,
-        Order: false,
-        Portfolio:false
+        leftHeader: 'Watchlists',
+        rightHeader: 'Edit',
+        bodyNumber: 1,
       });
 
-    case START_ORDER:
-    return Object.assign({},state,{
-      Order: true,
-      StockList: false,
-      Portfolio:false
-    });
+    case 2:
+      return Object.assign({},state,{
+        leftHeader: 'Edit Order',
+        rightHeader: '',
+        bodyNumber: 2,
+      });
+    case 3:
+      return Object.assign({},state,{
+        leftHeader: '',
+        rightHeader: '',
+        bodyNumber: 3,
+      });
+    case 4:
+      return Object.assign({},state,{
+        leftHeader: 'New',
+        rightHeader: 'Close',
+        bodyNumber: 4,
+      });
+    case 5:
+      return Object.assign({},state,{
+        // leftHeader: 'Watchlists',
+        // rightHeader: 'Edit',
+        bodyNumber: 5,
+      });
 
-    case START_PORTFOLIO:
-    return Object.assign({},state,{
-      Portfolio: true,
-      StockList: false,
-      Order: false,
-    });
+    case 6:
+      return Object.assign({},state,{
+        // leftHeader: 'Edit Order',
+        // rightHeader: '',
+        bodyNumber: 6,
+      });
+    case 7:
+      return Object.assign({},state,{
+        // leftHeader: '',
+        // rightHeader: '',
+        bodyNumber: 7,
+      });
+    case 8:
+      return Object.assign({},state,{
+        // leftHeader: 'New',
+        // rightHeader: 'Close',
+        bodyNumber: 8,
+      });
+    case 9:
+      return Object.assign({},state,{
+        // leftHeader: 'Watchlists',
+        // rightHeader: 'Edit',
+        bodyNumber: 9,
+      });
+
+    case 10:
+      return Object.assign({},state,{
+        // leftHeader: 'Edit Order',
+        // rightHeader: '',
+        bodyNumber: 10,
+      });
+    case 11:
+      return Object.assign({},state,{
+        // leftHeader: '',
+        // rightHeader: '',
+        bodyNumber: 11,
+      });
+    case 12:
+      return Object.assign({},state,{
+        // leftHeader: 'New',
+        // rightHeader: 'Close',
+        bodyNumber: 12,
+      });
+    case 13:
+      return Object.assign({},state,{
+        // leftHeader: '',
+        // rightHeader: '',
+        bodyNumber: 13,
+      });
+    case 14:
+      return Object.assign({},state,{
+        // leftHeader: 'New',
+        // rightHeader: 'Close',
+        bodyNumber: 14,
+      });
 
     default:
         return state
